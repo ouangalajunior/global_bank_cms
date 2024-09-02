a10e8998f9ba9da8c594b3eb73efa7aa26f33653
Junior Ouangala <ouangalajunior@MacBook-Air-de-Junior.local>
2024-09-02 15:34:38 +0200

    Create and style staff page

---
+++ public/staff/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GBI</title>
+</head>
+<body>
+    <header>
+        <h1>GBI Staff area</h1>
+        <link rel="stylesheet" href="../stylesheets/staff.css" media="all">
+    </header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Menu</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="content">
+
+    </div>
+
+    <footer>
+        &copy; <?php  echo date('Y');?>  Globe Bank
+    </footer>
+</body>
+</html>
